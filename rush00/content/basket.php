931ea249cc18bdd1d02a66037f860c59c9ecbb11
Johnathan Dwayne Heath <jheath@student.wethinkcode.co.za>
2018-10-07 16:33:58 +0200

    style and basket update

--- rush00/content/basket.php
+++ rush00/content/basket.php
@@ -1,32 +1,55 @@
-<?PHP
+<?php
+
 	session_start();
-	require_once('products.php');
+	include_once '../Extra/functions.php';
+	include_once '../Products/products.php'
+?>
 
-	if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['id'])
-	{
-		$movies = product_byid($_POST('id'));
-		if ($_POST['quantity'] && is_numeric($_POST['quantity']) && $_POST['quantity'] > 0 && $movies)
-		{
-			$basket = unserialize($_SESSION['basket_product']);
-			if ($basket[$_POST['id']])
-			{
-				$basket[$_POST['id']] += $_POST['quantity'];
-				$_SESSION['basket_count'] += $_POST['quantity'];
-				$_SESSION['basket_price'] += $movies['price'] * $_POST['quantity'];
-			}
-			else
-			{
-				$basket[$_POST['id'] = serialize($basket);
-				$_SESSION['basket_count'] += $_POST['quantity'];
-				$_SESSION['basket_price'] += $movies['price'] * $_POST['quantity'];
-			}
-			$_SESSION['basketMovie'] = serialize($basket);
-		}
-	}
-
-	if ($_GET['remove'] == '1') 
-	{
-		$_SESSION['basket_product'] = NULL;
-		$_SESSION['basket_price'] = NULL;
-		$_SESSION['basket_count'] = NULL;
-	}
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Index</title>
+	<link rel="stylesheet" href="../Style/style.css">
+</head>
+<?php ft_printhead('Title'); ?>
+<body class="body">
+	<?php ft_printheader(); ?>
+	<div class="maincontent">
+		<div class="container">
+			<h2>Basket</h2>
+			<table>
+			<?PHP 
+				if (addto_basket())
+				{
+					$articles = count($_SESSION['array']);
+					if ($atricles <= 0)
+						echo "<tr><td>Currently no items in basket</td></tr>";
+					else
+					{
+						for ($i = 0; $i < $articles; i++)
+						{
+							echo "<tr>";
+							echo "<td>"$_SESSION['product_name']"</td>";
+							echo "<td>"$_SESSION['quantity']"</td>"
+							echo "<td>"$_SESSION['price']"</td>";
+							echo "</tr>";
+						}
+					}
+				}
+			?>
+				<tr>
+					<td>$product_name:</td>
+				</tr>
+				<tr>
+					<td>$product_name:</td>
+				</tr>
+				<tr>
+					<td class="button"><input type="submit" name="OK" value="Confirm Order"></td>
+					<td><input type="submit" name="OK" value="Cancel Order"></td>
+				</tr>
+			</table>
+		</div>
+	</div>
+	<?php ft_printfooter(); ?>
+</body>
+</html>
